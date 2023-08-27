bc432312c839148c9a692bdac4423be8b3b461cb
Shashwat Shivhare <shashwat02infinity@gmail.com>
2023-08-27 15:34:56 +0530

    Laying the foundation

---
+++ Laying the Foundation/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement(
+    "h1",
+    {id:"hello",
+        style:{color : "red"},
+},
+"Hello World from react")
+const heading2 = React.createElement(
+    "h2",
+    {id:"subtitle"},
+    "my name is shashwat"
+)
+const container = React.createElement("div",
+{
+    id:"container"
+},
+[heading,heading2]
+)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(container)
